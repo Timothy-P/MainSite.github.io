b8201818e01f5b143db91f1eed9c968df60faaaa
Timothy <128176649+Timothy-P@users.noreply.github.com>
2023-11-13 17:15:36 +0000

    Small change.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function Alpha() {
     $("body").append('<div style="position: relative;width: 200px;height: 200px; background-color: coral;" id="Nav">')
     $("#Nav").append('<div style="width: auto; height: 50px; background-color: darkcyan;cursor: move;" id="TNav"></div>')
     $("#Nav").append('<button onclick="alert(\'Test complete\')">Test Button</button>')
-    $("#TNav").append('<button onclick="$(\'#Nav\').draggable(\'enabled\'); AlphaUL()">&#128275;</button>')
+    $("#TNav").append('<button onclick="$(\'#Nav\').draggable(\'enabled\'); AlphaL()">&#128275;</button>')
     $(function() {  
         $( "#Nav" ).draggable();  
     });
