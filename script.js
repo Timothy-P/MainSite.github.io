02d0b4d0e41a6dc629b1fb5c340c4736b0ff51a2
Timothy <128176649+Timothy-P@users.noreply.github.com>
2023-11-13 18:51:25 +0000

    Menu basics done.

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
+var NavLock = 0
+NavA = 0
 function Alpha() {
-    $("body").append('<div style="position: relative;width: 200px;height: 200px; background-color: coral;" id="Nav">')
-    $("#Nav").append('<div style="width: auto; height: 50px; background-color: darkcyan;cursor: move;" id="TNav"></div>')
-    $("#Nav").append('<button onclick="alert(\'Test complete\')">Test Button</button>')
-    $("#TNav").append('<button onclick="$(\'#Nav\').draggable(\'enabled\'); AlphaL()">&#128275;</button>')
-    $(function() {  
-        $( "#Nav" ).draggable();  
-    });
+    if (NavA == 0) {
+        $("body").append('<div style="position: relative;width: 200px;height: 200px; background-color: coral;opacity:1" id="Nav">')
+        $("#Nav").append('<div style="width: auto; height: 15px; background-color: darkcyan;cursor: move;padding:12px;" id="TNav"></div>')
+        $("#Nav").append('<button onclick="alert(\'Test complete\')">Test Button</button>')
+        $("#TNav").append('<button style="position:relative;left:33%;cursor:auto;background:transparent;border:none;" id="LockB" onclick="AlphaL()">&#128275;</button>')
+        $("#TNav").append('<button style="position:relative;left:32%;cursor:auto;background:transparent;border:none;" onclick="AlphaC()">&#10060;</button>')
+        $(function() {  
+            $("#Nav").draggable();  
+        });
+        NavA = 1
+    }
+    else {
+        alert("Menu is open.")
+    }
 }
 function AlphaC() {
     document.getElementById("Nav").remove()
+    NavLock = 0
+    NavA = 0
 }
 function AlphaL() {
     if (NavLock == 1) {
         $("#Nav").draggable({disabled:false})
+        NavLock = 0
+        document.getElementById("LockB").innerHTML = "&#128275;"
     }
     else {
-        $("#Nav").draggable({disabled:true})
+        $("#Nav").draggable({disabled:true});
+        NavLock = 1
+        document.getElementById("LockB").innerHTML = "&#128274;"
     }
 }
